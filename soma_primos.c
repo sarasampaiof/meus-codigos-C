6dfeb52c8688ef528553cd8195fcc07d87b7ee16
Sara Sampaio Figueredo <sarasamp1512@gmail.com>
2026-09-21 14:35:28 -0300

    Add program to sum first N prime numbers
    Implement a program to sum the first N prime numbers.

---
+++ soma_primos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define N 5
+int main() {
+    int num = 2, div, primo=1, qntd=0, soma=0;
+
+    while (qntd < N) {
+
+        primo=1;
+        for (div = 2; div < num && primo; div++) {
+            if (num % div == 0) {
+                primo = 0; // ou seja, NÃO primo
+            }
+        }
+        if (primo) {
+            soma += num;
+            qntd++;
+        }
+
+
+        num++;
+    }
+
+    printf("%d", soma);
+
+    return 0;
+}

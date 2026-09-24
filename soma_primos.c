0d5c63bdbb3ec62b2640f24d000c4c6f24441bb5
Sara Sampaio Figueredo <sarasamp1512@gmail.com>
2026-09-24 01:23:31 -0300

    Update soma_primos.c

--- soma_primos.c
+++ soma_primos.c
@@ -4,7 +4,7 @@ int main() {
     int num = 2, div, primo=1, qntd=0, soma=0;
 
     while (qntd < N) {
-        primo=1; // ou seja, primo = verdadeiro
+        primo = 1; // ou seja, primo = verdadeiro
         for (div = 2; div < num && primo; div++) {
             if (num % div == 0)
                 primo = 0; // ou seja, NÃO primo

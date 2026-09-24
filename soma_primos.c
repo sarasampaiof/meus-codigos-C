eb1c7fa732f2cc6ffa89386e98b6972144f084b7
Sara Sampaio Figueredo <sarasamp1512@gmail.com>
2026-09-24 01:21:40 -0300

    Update soma_primos.c
    Refactor prime-checking logic to simplify if statement.

--- soma_primos.c
+++ soma_primos.c
@@ -4,13 +4,12 @@ int main() {
     int num = 2, div, primo=1, qntd=0, soma=0;
 
     while (qntd < N) {
-
         primo=1;
         for (div = 2; div < num && primo; div++) {
-            if (num % div == 0) {
+            if (num % div == 0)
                 primo = 0; // ou seja, NÃO primo
-            }
         }
+        
         if (primo) {
             soma += num;
             qntd++;
